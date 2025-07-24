1900be9b2795add04c84dcaa766cd0afbaa78371
nanayose <nanayose.e7@gmail.com>
2025-07-25 00:33:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -97,4 +97,6 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="「おみくじ」と送ってくれたら運勢を占うよ！")
+
+            @app.route("/callback", methods=['POST'])
         )
